600ab3f51b271cc87ca4112c5f6fefc4d7a811e7
Stepa <stepakrutov030306@gmail.com>
2024-09-18 22:07:25 +0500

    fix bug

--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -4,10 +4,10 @@ import styles from "./styles.module.scss"
 import icon from "../../assets/trash.svg"
 import classNames from "classnames"
 
-export const Task: React.FC<Iprops> = ({text, onDelete,onToggle, completed }) => {
+export const Task: React.FC<Iprops> = ({text, onDelete,onToggle, completed}) => {
     return (
         <div onClick={onToggle} className={classNames(styles.item, {[styles.completed]: completed})}>
-            <input className={styles.checkbox} type="checkbox" onChange={onToggle} checked={completed}/>
+            <input className={styles.checkbox} type="checkbox" readOnly checked={completed}/>
             <p className={styles.text}>{text}</p>
             <button onClick={onDelete}><Image className={styles.delete} src={icon} alt="trash"/></button>
         </div>
